46569487e3a7c73a098ca927d65708f4607064e9
godfoca <godfoca@c62ddfc6-5434-0410-8a9f-6560581ae375>
2007-12-23 04:37:00 +0000

    Changed Number extensions to be run inside anonymous functions so they don't clutter the namespace
    git-svn-id: http://active-support-for-javascript.googlecode.com/svn/trunk@5 c62ddfc6-5434-0410-8a9f-6560581ae375

--- src/number.js
+++ src/number.js
@@ -11,36 +11,38 @@ Object.extend(Number.prototype, {
 	}
 });
 
-Number.ByteExtensions = {
-	byte:     function() { return this; },
-	kilobyte: function() { return this * 1024; },
-	megabyte: function() { return this * (1024).kilobytes(); },
-	gigabyte: function() { return this * (1024).megabytes(); },
-	terabyte: function() { return this * (1024).gigabytes(); },
-	petabyte: function() { return this * (1024).terabytes(); },
-	exabyte:  function() { return this * (1024).petabytes(); }
-};
-ActiveSupport.pluralizeMethods(Number.ByteExtensions);
-Object.extend(Number.prototype, Number.ByteExtensions);
+(function() {
+	var ByteExtensions = {
+		byte:     function() { return this; },
+		kilobyte: function() { return this * 1024; },
+		megabyte: function() { return this * (1024).kilobytes(); },
+		gigabyte: function() { return this * (1024).megabytes(); },
+		terabyte: function() { return this * (1024).gigabytes(); },
+		petabyte: function() { return this * (1024).terabytes(); },
+		exabyte:  function() { return this * (1024).petabytes(); }
+	};
+	ActiveSupport.pluralizeMethods(ByteExtensions);
+	Object.extend(Number.prototype, ByteExtensions);
 
-Number.IntervalExtensions = {
-  second:    function() { return this * 1000; },
-  minute:    function() { return this.seconds() * 60; },
-  hour:      function() { return this.minutes() * 60; },
-  day:       function() { return this.hours() * 24; },
-  week:      function() { return this.days() * 7; },
-	fortnight: function() { return this.weeks() * 2; },
-	month:     function() { return this.days() * 30; },
-	year:      function() { return this.months() * 12 }
-};
-ActiveSupport.pluralizeMethods(Number.IntervalExtensions);
-Object.extend(Number.prototype, Number.IntervalExtensions);
-
-Number.TimeExtensions = {
-  since: function(reference) { return new Date((reference || new Date()).getTime() + this); },
-  until: function(reference) { return new Date((reference || new Date()).getTime() - this); }
-};
-Number.TimeExtensions.toDate = (0).ago;
-Number.TimeExtensions.fromNow = Number.TimeExtensions.since.curry(null);
-Number.TimeExtensions.ago = Number.TimeExtensions.until.curry(null);
-Object.extend(Number.prototype, Number.TimeExtensions);
+	var IntervalExtensions = {
+	  second:    function() { return this * 1000; },
+	  minute:    function() { return this.seconds() * 60; },
+	  hour:      function() { return this.minutes() * 60; },
+	  day:       function() { return this.hours() * 24; },
+	  week:      function() { return this.days() * 7; },
+		fortnight: function() { return this.weeks() * 2; },
+		month:     function() { return this.days() * 30; },
+		year:      function() { return this.months() * 12 }
+	};
+	ActiveSupport.pluralizeMethods(IntervalExtensions);
+	Object.extend(Number.prototype, IntervalExtensions)
+	
+	var TimeExtensions = {
+	  since: function(reference) { return new Date((reference || new Date()).getTime() + this); },
+	  until: function(reference) { return new Date((reference || new Date()).getTime() - this); }
+	}
+	TimeExtensions.fromNow = TimeExtensions.since.curry(null);
+	TimeExtensions.ago = TimeExtensions.until.curry(null);
+	TimeExtensions.toDate = (0).ago;
+	Object.extend(Number.prototype, TimeExtensions);
+})();
